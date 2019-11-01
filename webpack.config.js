f8288d24b8b849943cbfdd088fda38e9940bf4dd
dinusha-b14 <dinushab@jobready.com.au>
2019-11-02 00:22:51 +1100

    setup boilerplate react app

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+'use strict';
+
+const path = require('path');
+
+module.exports = {
+    entry: './src/app.js',
+    output: {
+        path: path.join(__dirname, 'public'),
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [{
+            test: /\.js$/,
+            exclude: /node_modules/,
+            loader: 'babel-loader'
+        }, {
+            test: /\.s?css$/,
+            use: [
+                'style-loader',
+                'css-loader',
+                'sass-loader'
+            ]
+        }]
+    },
+    devtool: 'cheap-module-eval-source-map',
+    devServer: {
+        contentBase: path.join(__dirname, 'public')
+    },
+    mode: process.env.NODE_ENV || 'development'
+};
